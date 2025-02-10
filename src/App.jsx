458bc685b7e5af748d9ff3c70a9ca2fe1768c773
saritacoder <160239576+saritacoder@users.noreply.github.com>
2025-02-10 12:30:06 +0530

    forgot-password

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AuthContext from './context/AuthContext';
 import Profile from './components/profile/Profile';
 import Header from './components/header/Heade';
 import Signup from './components/login/Signup';
+import ForgotPassword from './components/forgotpassword/ForgotPassword';
 
 function App() {
 const authCtx = useContext(AuthContext);
@@ -15,6 +16,9 @@ const authCtx = useContext(AuthContext);
     <Header>
 <Routes>
 <Route path="/" element={<Signup />} />
+<Route path="/login" element={<Signup />} />
+<Route path="/forgot-password" element={<ForgotPassword />} />
+
 <Route   path="/profile"  element={authCtx.isLoggedIn ? <Profile /> : <Navigate to="/login" />}      />     
              
            
